6976adffb8be40750efddb12d3802aa76444707e
Budi Erwanto <budi@call-levels.com>
2018-11-23 14:43:27 +0700

    UI Add Guest List

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import ChatText from "./component/ChatText";
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+      </div>
     );
   }
 }
---
+++ src/component/GuestList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const guestList = (props) => {
+    return (
+        <div className="col-md-2 offset-md-10">
+            {
+                props.guests.map((g) => (
+                    <div key={g} className="m-3">{g}</div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default guestList;
