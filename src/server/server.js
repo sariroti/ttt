a0dcaf728f2486399dec71721d0af262659b527e
Budi Erwanto <budi@call-levels.com>
2018-11-23 15:30:20 +0700

    Add and Setup Socket.IO

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,16 @@
-const express = require("express");
-const app = express();
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 app.get("/", (req,res) => {
     res.send("hello");
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
 })
 
-app.listen(3000, () => {
+io.listen(3001);
+http.listen(3000, () => {
     console.log("server started!");
-} )
+});
