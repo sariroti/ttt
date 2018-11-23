2e0a36a698e329ef1263b94aab3d032178b1c7d5
Budi Erwanto <budi@call-levels.com>
2018-11-23 14:58:44 +0700

    UI Fix Position Sticky Top

--- src/component/GuestList.js
+++ src/component/GuestList.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const guestList = (props) => {
     return (
-        <div className="col-md-2 offset-md-10">
+        <div className="col-md-2 offset-md-10 fixed-top">
             {
                 props.guests.map((g) => (
                     <div key={g} className="m-3">{g}</div>
